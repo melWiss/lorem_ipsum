d930a670181f37db2aa9c0640fdb3dd4d49b77fc
Zahra Khf <zahrakhalifa190@gmail.com>
2021-03-07 13:39:16 +0100

    added carte && emergency

---
+++ lib/emergency.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class Emergencyclass extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            InkWell(
+              child: Material(
+                elevation: 4,
+                borderRadius: BorderRadius.circular(200),
+                color: Colors.pink[900],
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 40,
+                    vertical: 80,
+                  ),
+                  child: Text(
+                    "help me",
+                    style: TextStyle(fontSize: 40, color: Colors.white),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              child: Text(
+                "Emergency call",
+                style: TextStyle(fontSize: 40, color: Colors.black),
+              ),
+              padding: EdgeInsets.all(20),
+            )
+          ],
+        ),
+      ),
+      backgroundColor: Colors.pink.shade100,
+    );
+  }
+}
