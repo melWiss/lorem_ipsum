03a4b814a769712604440a82dadf94c96ecaa8c1
Oussama Maatallah <oussamamaatallah01@gmail.com>
2021-03-09 19:13:08 +0100

    finished profile screen.

---
+++ lib/assets.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Assets {
+  static Color primary = Colors.pink;
+  static Color secondary = Colors.pink.shade100;
+  static TextStyle title = TextStyle(
+    fontWeight: FontWeight.bold,
+    fontSize: 42,
+  );
+  static TextStyle subTitle = TextStyle(
+    fontWeight: FontWeight.bold,
+    fontSize: 22,
+  );
+}
